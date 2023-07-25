ca9fa007429f33038bae11cf48a0ff899a3f03a9
Riley <ririwilliams.rw@gmail.com>
2023-07-24 19:52:41 -0500

    Added dynamic listing of Astronauts, and from most to least hours in space

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-// TODO: add code here
+
+
+window.addEventListener("load", () => {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then((response) => {
+        response.json().then((astronauts) => {
+        let div = document.getElementById("container");
+
+        let sortedHours = astronauts.sort((a1, a2) => (a1.hoursInSpace < a2.hoursInSpace) ? 1 : (a1.hoursInSpace > a2.hoursInSpace) ? -1 : 0);
+        // console.log(sortedHours);
+        // console.log(astronauts);
+        for (let i = 0; i < astronauts.length; i++){
+            let astronaut = astronauts[i];
+
+            div.innerHTML += (`
+            <div class="astronaut">
+            <div class="bio">
+                <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+                    <ul>
+                        <li>Hours in space: ${astronaut.hoursInSpace}</li>
+                        <li>Active: ${astronaut.active}</li>
+                        <li>Skills: ${astronaut.skills}</li>
+                    </ul>
+            </div>
+            <img class="avatar" src="${astronaut.picture}">
+            </div>`)
+        }
+
+        // div.innerHTML = `
+        // <div class="astronaut">
+        // <div class="bio">
+        //     <h3>${astronauts[i].firstName} ${astronauts[i].lastName}</h3>
+        //         <ul>
+        //             <li>Hours in space: 190</li>
+        //             <li>Active: false</li>
+        //             <li>Skills: Physician, Chemical Engineer</li>
+        //         </ul>
+        // </div>
+        // <img class="avatar" src="images/mae-jemison.jpg">
+        // </div>
+        // `
+        })
+    })
+})
